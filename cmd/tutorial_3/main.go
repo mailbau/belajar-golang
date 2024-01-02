34e5f5a4fccbfef53a34b5e54f04f4791b699d2b
mailbau <simailpipiw@gmail.com>
2024-01-02 14:10:40 +0700

    feat: complete tutorial_3

--- cmd/tutorial_3/main.go
+++ cmd/tutorial_3/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
 	var printValue string = "Hello World"
@@ -8,16 +11,45 @@ func main() {
 
 	var numerator int = 11
 	var denominator int = 2
-	var result, remainder int = intDivision(numerator, denominator)
-	fmt.Printf("The result of division is %v with remainder %v", result, remainder)
+	var result, remainder, err = intDivision(numerator, denominator)
+	/*if err != nil {
+		fmt.Printf(err.Error())
+	} else if remainder == 0 {
+		fmt.Printf("The result of division is %v", result)
+	} else {
+		fmt.Printf("The result of division is %v with remainder %v", result, remainder)
+	}*/
+	switch {
+	case err != nil:
+		fmt.Printf(err.Error())
+	case remainder == 0:
+		fmt.Printf("The result of division is %v", result)
+	default:
+		fmt.Printf("The result of division is %v with remainder %v", result, remainder)
+	}
+
+	switch remainder {
+	case 0:
+		fmt.Printf("The division was exact")
+	case 1, 2:
+		fmt.Printf("The division was close")
+	default:
+		fmt.Printf("The division was not close")
+	}
 }
 
 func printMe(printValue string) {
 	fmt.Println(printValue)
 }
 
-func intDivision(numerator int, denominator int) (int, int) {
+func intDivision(numerator int, denominator int) (int, int, error) {
+	var err error
+	if denominator == 0 {
+		err = errors.New("Cannot divide by zero")
+		return 0, 0, err
+	}
+
 	var result int = numerator / denominator
 	var remainder int = numerator % denominator
-	return result, remainder
+	return result, remainder, err
 }
